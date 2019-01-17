b301dafa98f77c33fe85890a2f6cc0c949dcbf92
Jan <jan@hettenkofer.net>
2019-01-17 17:24:49 +0100

    Refactored generalised pseudo-Matrix code into proper, 2D-only Matrix class

--- src/prototype.h
+++ src/prototype.h
@@ -1,40 +1,45 @@
+#include <iostream>
+
 #include <vector>
 #include <array>
 #include <type_traits>
 
 using namespace std;
 
-template <class T, unsigned int num_dims = 1>
+template <class T, unsigned int w = 1, unsigned int h = 1>
 class Matrix {
 public:
-	/**
-	 * Contains the size of each of the dimensions.
-	 * e.g.: {512, 512, 4} for a 512 x 512 pixel RGBA image
-	 * or { 4, 4 } for a transformation matrix in 3D space
-	 *
-	 * Must be assigned in the constructor.
-	 */
-    const std::array<const unsigned int, num_dims> dimensions;
-    
     std::vector<T> data;
 
-    Matrix(const std::array<const unsigned int, num_dims>&& dimensions):
-    dimensions(dimensions)
+    const unsigned int width = w;
+    const unsigned int height = h;
+
+    Matrix():
+	data(std::vector<T>(w * h))
     {
-    	unsigned int size = 1;
-    	for (unsigned int dim: dimensions) size *= dim;
-    	data = std::vector<T>(size);
     }
 
     ~Matrix() {
 
     }
 
-	/**
+    T& at (const unsigned int x, const unsigned int y) {
+    	// TODO: bounds checking
+    	// static_assert(x * w + y <= w * h, "Cannot access coordinates outside Matrix.");
+    	return data[x * w + y];
+    }
+
+    T at (const unsigned int x, const unsigned int y) const {
+    	// TODO: bounds checking
+    	// static_assert(x * w + y <= w * h, "Cannot access coordinates outside Matrix.");
+    	return data[x * w + y];
+    }
+
+    /**
 	 * Adds a matrix to another.
-	 * For now, adding matrices of differing sizes is undefined behavior.
+	 * For now, adding matrices off differing sizes is undefined behavior/unsupported.
 	 */
-    Matrix& operator+= (const Matrix<T, num_dims>& rhs) {
+    Matrix& operator+= (const Matrix<T, w, h>& rhs) {
     	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
 		for (unsigned int i = 0; i < data.size(); ++i) {
 			data[i] += rhs.data[i];
@@ -44,9 +49,9 @@ public:
 
 	/**
 	 * Adds two matrices together.
-	 * For now, adding matrices of differing sizes is undefined behavior.
+	 * For now, adding matrices of differing sizes is undefined behavior/unsupported.
 	 */
-	friend Matrix operator+ (Matrix lhs, const Matrix& rhs) {
+	friend Matrix operator+ (Matrix<T, w, h> lhs, const Matrix<T, w, h>& rhs) {
 		lhs += rhs;
 		return lhs;
 	}
@@ -68,13 +73,137 @@ public:
 
 	/**
 	 * Adds two matrices together.
+	 * For now, adding matrices of differing sizes is undefined behavior/unsupported.
+	 */
+	template<
+    	typename N,
+    	typename = typename std::enable_if<std::is_arithmetic<N>::value, N>::type
+	>
+	friend Matrix operator+ (Matrix<T, w, h> lhs, const N& rhs) {
+		lhs += rhs;
+		return lhs;
+	}
+
+	/**
+	 * Subtracts a matrix from another.
+	 * For now, subtracting matrices of differing sizes is undefined behavior/unsupported.
+	 */
+    Matrix& operator-= (const Matrix<T, w, h>& rhs) {
+    	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
+		for (unsigned int i = 0; i < data.size(); ++i) {
+			data[i] -= rhs.data[i];
+		}
+		return *this;
+	}
+
+	/**
+	 * Subtracts two matrices from one another.
+	 * For now, subtracting matrices of differing sizes is undefined behavior/unsupported.
+	 */
+	friend Matrix operator- (Matrix<T, w, h> lhs, const Matrix<T, w, h>& rhs) {
+		lhs -= rhs;
+		return lhs;
+	}
+
+	/**
+	 * Multiplies two matrices together.
+	 * TODO: check if this can be optimised.
+	 */
+	template<
+		unsigned int lhs_height, unsigned int lhs_width,
+		unsigned int rhs_height, unsigned int rhs_width
+	>
+	friend Matrix<T, rhs_width, lhs_height> operator* (
+		const Matrix<T, lhs_width, lhs_height>& lhs, const Matrix<T, rhs_width, rhs_height>& rhs
+	) {
+    	static_assert(rhs_height == lhs_width, "Righthand side Matrix height must equal lefthand side Matrix width.");
+
+    	Matrix<T, rhs_width, lhs_height> result;
+
+    	for (unsigned int x = 0; x < rhs_width; ++x) {
+    		for (unsigned int y = 0; y < lhs_height; ++y) {
+    			T entry;
+    			for (unsigned int i = 0; i < lhs_width; ++i) {
+    				entry += lhs.at(y, i) * rhs.at(i, x);
+    			}
+    			result.at(x, y) = entry;
+    		}
+    	}
+		return result;
+	}
+};
+
+template <class T, unsigned int num_dims = 1>
+class Tensor {
+public:
+	/**
+	 * Contains the size of each of the dimensions.
+	 * e.g.: {512, 512, 4} for a 512 x 512 pixel RGBA image
+	 * or { 4, 4 } for a transformation matrix in 3D space
+	 *
+	 * Must be assigned in the constructor.
+	 */
+    const std::array<const unsigned int, num_dims> dimensions;
+    
+    std::vector<T> data;
+
+    Tensor(const std::array<const unsigned int, num_dims>&& dimensions):
+    dimensions(dimensions)
+    {
+    	unsigned int size = 1;
+    	for (unsigned int dim: dimensions) size *= dim;
+    	data = std::vector<T>(size);
+    }
+
+    ~Tensor() {
+
+    }
+
+	/**
+	 * Adds a matrix to another.
+	 * For now, adding matrices of differing sizes is undefined behavior.
+	 */
+    Tensor& operator+= (const Tensor<T, num_dims>& rhs) {
+    	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
+		for (unsigned int i = 0; i < data.size(); ++i) {
+			data[i] += rhs.data[i];
+		}
+		return *this;
+	}
+
+	/**
+	 * Adds two matrices together.
+	 * For now, adding matrices of differing sizes is undefined behavior.
+	 */
+	friend Tensor operator+ (Tensor lhs, const Tensor& rhs) {
+		lhs += rhs;
+		return lhs;
+	}
+
+	/**
+	 * Adds a scalar to all components of a matrix.
+	 */
+	template<
+    	typename N,
+    	typename = typename std::enable_if<std::is_arithmetic<N>::value, N>::type
+	>
+    Tensor& operator+= (const N& rhs) {
+    	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
+		for (unsigned int i = 0; i < data.size(); ++i) {
+			data[i] += rhs;
+		}
+		return *this;
+	}
+
+	/**
+	 * Adds two matrices together.
 	 * For now, adding matrices of differing sizes is undefined behavior.
 	 */
 	template<
     	typename N,
     	typename = typename std::enable_if<std::is_arithmetic<N>::value, N>::type
 	>
-	friend Matrix operator+ (Matrix lhs, const N& rhs) {
+	friend Tensor operator+ (Tensor lhs, const N& rhs) {
 		lhs += rhs;
 		return lhs;
 	}
@@ -83,7 +212,7 @@ public:
 	 * Subtracts a matrix from another.
 	 * For now, subtracting matrices of differing sizes is undefined behavior.
 	 */
-    Matrix& operator-= (const Matrix<T, num_dims>& rhs) {
+    Tensor& operator-= (const Tensor<T, num_dims>& rhs) {
     	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
 		for (unsigned int i = 0; i < data.size(); ++i) {
 			data[i] -= rhs.data[i];
@@ -95,17 +224,17 @@ public:
 	 * Subtracts two matrices from one another.
 	 * For now, subtracting matrices of differing sizes is undefined behavior.
 	 */
-	friend Matrix operator- (Matrix lhs, const Matrix& rhs) {
+	friend Tensor operator- (Tensor lhs, const Tensor& rhs) {
 		lhs -= rhs;
 		return lhs;
 	}
 
 	/**
-	 * TODO: IMpleent ACTUAL matrix multiplication
+	 * TODO: Implement ACTUAL matrix multiplication
 	 * Multiplies a matrix with another.
 	 * For now, multiplying matrices of differing sizes is undefined behavior.
 	 */
-    Matrix& operator*= (const Matrix<T, num_dims>& rhs) {
+    Tensor& operator*= (const Tensor<T, num_dims>& rhs) {
     	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
 		for (unsigned int i = 0; i < data.size(); ++i) {
 			data[i] *= rhs.data[i];
@@ -117,7 +246,7 @@ public:
 	 * Multiplies two matrices together.
 	 * For now, multiplying matrices of differing sizes is undefined behavior.
 	 */
-	friend Matrix operator* (Matrix lhs, const Matrix& rhs) {
+	friend Tensor operator* (Tensor lhs, const Tensor& rhs) {
 		lhs *= rhs;
 		return lhs;
 	}
@@ -126,7 +255,7 @@ public:
 	 * Divides a matrix by another.
 	 * For now, dividing matrices of differing sizes as well as dividing by matrices containing 0 is undefined behavior.
 	 */
-    Matrix& operator/= (const Matrix<T, num_dims>& rhs) {
+    Tensor& operator/= (const Tensor<T, num_dims>& rhs) {
     	// todo: iterate in a way that allows checking if rhs is defined at that particular position and to use 0 in that case
 		for (unsigned int i = 0; i < data.size(); ++i) {
 			data[i] /= rhs.data[i];
@@ -138,7 +267,7 @@ public:
 	 * Divides one matrix by another.
 	 * For now, dividing matrices of differing sizes as well as dividing by matrices containing 0 is undefined behavior.
 	 */
-	friend Matrix operator/ (Matrix lhs, const Matrix& rhs) {
+	friend Tensor operator/ (Tensor lhs, const Tensor& rhs) {
 		lhs /= rhs;
 		return lhs;
 	}
@@ -146,7 +275,7 @@ public:
 	unsigned int indexAt(const std::vector<unsigned int> &coordinates) const {
 		unsigned int idx = 0;
 		for (unsigned int i = 0; i < coordinates.size(); ++i) {
-			// increment index by coordinate (aka line/colomn number) multiplied by the size of the Matrix in that dimension
+			// increment index by coordinate (aka line/colomn number) multiplied by the size of the Tensor in that dimension
 			idx += coordinates[i] * dimensions[i];
 		}
 		return idx;
--- tests/prototype.test.cpp
+++ tests/prototype.test.cpp
@@ -5,15 +5,39 @@
 
 #include "../src/prototype.h"
 
-TEST(Matrix, MatrixConstructor) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+TEST(Matrix, AccessAt) {
+	Matrix<int, 2, 2> m;
+	m.data[2] = 42;
+
+	// check if accessing works
+	EXPECT_EQ(0, m.at(0, 0));
+	EXPECT_EQ(0, m.at(0, 1));
+	EXPECT_EQ(42, m.at(1, 0));
+	EXPECT_EQ(0, m.at(1, 1));
+	
+	// check if assigning works
+	m.at(0,1) = 47;
+	EXPECT_EQ(47, m.at(0, 1));
+}
+
+TEST(Matrix, OperatorMultiply) {
+	Matrix<int, 2, 3> m;
+	Matrix<int, 3, 2> n;
+
+	Matrix result = m * n;
+	EXPECT_EQ(result.height, m.height);
+	EXPECT_EQ(result.width, n.width);
+}
+
+TEST(Tensor, TensorConstructor) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
 
 	EXPECT_EQ(27, m.data.size());
 }
 
-TEST(Matrix, OperatorAddEquals) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorAddEquals) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 4;
 	n.data[0] = 2;
@@ -28,16 +52,16 @@ TEST(Matrix, OperatorAddEquals) {
 	EXPECT_NE(m.data, n.data);
 }
 
-TEST(Matrix, OperatorAdd) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorAdd) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 4;
 	n.data[0] = 2;
 
 	EXPECT_EQ(m.dimensions, n.dimensions);
 
-	Matrix<float, 3> res = m + n;
+	Tensor<float, 3> res = m + n;
 
 	EXPECT_NE(&m, &res);
 	EXPECT_NE(&n, &res);
@@ -46,8 +70,8 @@ TEST(Matrix, OperatorAdd) {
 	EXPECT_NE(m.data, res.data);
 }
 
-TEST(Matrix, OperatorScalarAddEquals) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorScalarAddEquals) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
 	unsigned int n = 42;
 
 	m.data[0] = 5;
@@ -57,13 +81,13 @@ TEST(Matrix, OperatorScalarAddEquals) {
 	EXPECT_EQ(47, m.data[0]);
 }
 
-TEST(Matrix, OperatorScalarAdd) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorScalarAdd) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
 	unsigned int n = 42;
 
 	m.data[0] = 5;
 
-	Matrix<float, 3> res = m + n;
+	Tensor<float, 3> res = m + n;
 
 	EXPECT_NE(&m, &res);
 	EXPECT_EQ(47, res.data[0]);
@@ -71,9 +95,9 @@ TEST(Matrix, OperatorScalarAdd) {
 	EXPECT_NE(m.data, res.data);
 }
 
-TEST(Matrix, OperatorSubtractEquals) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorSubtractEquals) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 42;
 	n.data[0] = 2;
@@ -88,16 +112,16 @@ TEST(Matrix, OperatorSubtractEquals) {
 	EXPECT_NE(m.data, n.data);
 }
 
-TEST(Matrix, OperatorSubtract) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorSubtract) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 42;
 	n.data[0] = 2;
 
 	EXPECT_EQ(m.dimensions, n.dimensions);
 
-	Matrix<float, 3> res = m - n;
+	Tensor<float, 3> res = m - n;
 
 	EXPECT_NE(&m, &res);
 	EXPECT_NE(&n, &res);
@@ -106,9 +130,9 @@ TEST(Matrix, OperatorSubtract) {
 	EXPECT_NE(m.data, res.data);
 }
 
-TEST(Matrix, OperatorMultiplyEquals) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorMultiplyEquals) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 4;
 	n.data[0] = 2;
@@ -123,16 +147,16 @@ TEST(Matrix, OperatorMultiplyEquals) {
 	EXPECT_NE(m.data, n.data);
 }
 
-TEST(Matrix, OperatorMultiply) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorMultiply) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 4;
 	n.data[0] = 2;
 
 	EXPECT_EQ(m.dimensions, n.dimensions);
 
-	Matrix<float, 3> res = m * n;
+	Tensor<float, 3> res = m * n;
 
 	EXPECT_NE(&m, &res);
 	EXPECT_NE(&n, &res);
@@ -141,9 +165,9 @@ TEST(Matrix, OperatorMultiply) {
 	EXPECT_NE(m.data, res.data);
 }
 
-TEST(Matrix, OperatorDivideEquals) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorDivideEquals) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 4;
 	for (int i = 0; i < n.data.size(); ++i) n.data[i] = 2;	// make sure we're not dividing by 0
@@ -158,16 +182,16 @@ TEST(Matrix, OperatorDivideEquals) {
 	EXPECT_NE(m.data, n.data);
 }
 
-TEST(Matrix, OperatorDivide) {
-	Matrix<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
-	Matrix<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
+TEST(Tensor, OperatorDivide) {
+	Tensor<float, 3> m(std::array<const unsigned int, 3>{3,3,3});
+	Tensor<float, 3> n(std::array<const unsigned int, 3>{3,3,3});
 
 	m.data[0] = 4;
 	for (int i = 0; i < n.data.size(); ++i) n.data[i] = 2;	// make sure we're not dividing by 0
 
 	EXPECT_EQ(m.dimensions, n.dimensions);
 
-	Matrix<float, 3> res = m / n;
+	Tensor<float, 3> res = m / n;
 
 	EXPECT_NE(&m, &res);
 	EXPECT_NE(&n, &res);
@@ -176,9 +200,9 @@ TEST(Matrix, OperatorDivide) {
 	EXPECT_NE(m.data, res.data);
 }
 
-// TEST(Matrix, MatrixAddEqualsPerformance) {
-// 	Matrix<float, 2> m(std::array<const unsigned int, 2>{14204, 10652});
-// 	Matrix<float, 2> n(std::array<const unsigned int, 2>{14204, 10652});
+// TEST(Tensor, TensorAddEqualsPerformance) {
+// 	Tensor<float, 2> m(std::array<const unsigned int, 2>{14204, 10652});
+// 	Tensor<float, 2> n(std::array<const unsigned int, 2>{14204, 10652});
 
 // 	std::cout << "Randomising data of " << m.data.size() + n.data.size() << " values." << std::endl;
 // 	for (int i = 0; i < m.data.size(); ++i) {
