a6e7b24e19d260f999d4b9b5404fde01190c968b
Jan Hettenkofer <jan@hettenkofer.net>
2019-02-04 22:37:41 +0100

    [ feature ] fleshed out and corrected unit tests for Node IO classes

--- src/graph/node_io.h
+++ src/graph/node_io.h
@@ -5,44 +5,68 @@
 #include <memory>
 #include <vector>
 
+#include <iostream>
+
 template <class T>
 class Output;
 
 template <class T>
 class Input: public std::enable_shared_from_this<Input<T>> {
 public:
-	std::string label;
-	std::weak_ptr<T> data;
-	std::weak_ptr<Output<T>> source;
+    std::string label;
+    std::weak_ptr<T> data;
+    std::weak_ptr<Output<T>> source;
 
-	void _connect(const std::shared_ptr<Output<T>> source) {
-		this->source = source;
-		data = source->data;
-	}
+    void _connect(const std::shared_ptr<Output<T>> source) {
+        this->source = source;
+        data = source->data;
+    }
 
-	void connect(std::weak_ptr<Output<T>> source) {
-		const auto source_sp = source.lock();
-		_connect(source_sp);
-		source_sp->_connect(this->shared_from_this());
-	}
+    void connect(std::shared_ptr<Output<T>> source) {
+        _connect(source);
+        source->_connect(this->shared_from_this());
+    }
+
+    void _disconnect() {
+        source.reset();
+        data.reset();
+    }
+
+    void disconnect() {
+        source.lock()->_disconnect(this->shared_from_this());
+        _disconnect();
+    }
 };
 
 template <class T>
 class Output: public std::enable_shared_from_this<Output<T>> {
 public:
-	std::string label;
-	std::shared_ptr<T> data;
-	std::vector<std::weak_ptr<Input<T>>> consumers;
+    std::string label;
+    std::shared_ptr<T> data;
+    std::vector<std::weak_ptr<Input<T>>> consumers;
 
-	void _connect(const std::shared_ptr<Input<T>> consumer) {
-		consumers.push_back(consumer);
-	}
+    void _connect(const std::shared_ptr<Input<T>> consumer) {
+        consumers.push_back(consumer);
+    }
 
-	void connect(std::weak_ptr<Input<T>> consumer) {
-		const auto consumer_sptr = consumer.lock();
-		_connect(consumer_sptr);
-		consumer_sptr->_connect(this->shared_from_this());
-	}
+    void connect(std::shared_ptr<Input<T>> consumer) {
+        _connect(consumer);
+        consumer->_connect(this->shared_from_this());
+    }
+
+    void _disconnect(std::shared_ptr<Input<T>> consumer) {
+        for (auto it = consumers.begin(); it != consumers.end(); ) {
+            if (it->lock() == consumer) {
+                consumers.erase(it);
+                break;
+            } else ++it;
+        }
+    }
+
+    void disconnect(std::shared_ptr<Input<T>> consumer) {
+        _disconnect(consumer);
+        consumer->disconnect();
+    }
 };
 
 #endif // SPICE_NODE_IO
--- tests/graph/node_io.test.cpp
+++ tests/graph/node_io.test.cpp
@@ -4,60 +4,128 @@
 
 #include "../../src/graph/node_io.h"
 
+// --------- //
+//   Input   //
+// --------- //
+
 TEST(Input, _Connect) {
-	auto i = std::make_shared<Input<int>>(Input<int>());
-	auto o = std::make_shared<Output<int>>(Output<int>());
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
 
-	i->_connect(o);
+    i->_connect(o);
 
-	// input's source address should equal output's address
-	EXPECT_EQ(o.get(), i->source.lock().get());
-	// input's address should be referred to by output's first entry into consumers vector
-	EXPECT_EQ(i.get(), o->consumers[0].lock().get());
-	// both input and output should reference the same address with their data property
-	EXPECT_EQ(o->data.get(), i->data.lock().get());
+    // input's source address should equal output's address
+    EXPECT_EQ(o.get(), i->source.lock().get());
+    // both input and output should reference the same address with their data property
+    EXPECT_EQ(o->data.get(), i->data.lock().get());
 }
 
 TEST(Input, Connect) {
-	auto i = std::make_shared<Input<int>>(Input<int>());
-	auto o = std::make_shared<Output<int>>(Output<int>());
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
 
-	i->connect(o);
+    i->connect(o);
 
-	// input's source address should equal output's address
-	EXPECT_EQ(o.get(), i->source.lock().get());
-	// output should have exactly one consumer
-	EXPECT_EQ(1, o->consumers.size());
-	// input's address should be referred to by output's first entry into consumers vector
-	EXPECT_EQ(i.get(), o->consumers[0].lock().get());
-	// both input and output should reference the same address with their data property
-	EXPECT_EQ(o->data.get(), i->data.lock().get());
+    // input's source address should equal output's address
+    EXPECT_EQ(o.get(), i->source.lock().get());
+    // output should have exactly one consumer
+    EXPECT_EQ(1, o->consumers.size());
+    // input's address should be referred to by output's first entry into consumers vector
+    EXPECT_EQ(i.get(), o->consumers[0].lock().get());
+    // both input and output should reference the same address with their data property
+    EXPECT_EQ(o->data.get(), i->data.lock().get());
 }
 
-// --------------------
+TEST(Input, _Disconnect) {
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
+    i->connect(o);
+
+    i->_disconnect();
+
+    // input's source address should be nullptr
+    EXPECT_EQ(nullptr, i->source.lock());
+    // output should still have exactly one consumer
+    EXPECT_EQ(1, o->consumers.size());
+    // input's address should still be referred to by output's first entry into consumers vector
+    EXPECT_EQ(i.get(), o->consumers[0].lock().get());
+    // input should not hold a reference to data anymore
+    EXPECT_EQ(nullptr, i->data.lock());
+}
+
+TEST(Input, Disconnect) {
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
+    i->connect(o);
+
+    i->disconnect();
+
+    // input's source address should be nullptr
+    EXPECT_EQ(nullptr, i->source.lock());
+    // output should now have zero consumers
+    EXPECT_EQ(0, o->consumers.size());
+    // both input and output should reference the same address with their data property
+    EXPECT_EQ(nullptr, i->data.lock());
+}
+
+// ---------- //
+//   Output   //
+// ---------- //
 
 TEST(Output, _Connect) {
-	auto i = std::make_shared<Input<int>>(Input<int>());
-	auto o = std::make_shared<Output<int>>(Output<int>());
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
 
-	o->_connect(i);
+    o->_connect(i);
 
-	// output should have exactly one consumer
-	EXPECT_EQ(1, o->consumers.size());
+    // output should have exactly one consumer
+    EXPECT_EQ(1, o->consumers.size());
+    // input's address should be referred to by output's first entry into consumers vector
+    EXPECT_EQ(i.get(), o->consumers[0].lock().get());
 }
 
 TEST(Output, Connect) {
-	auto i = std::make_shared<Input<int>>(Input<int>());
-	auto o = std::make_shared<Output<int>>(Output<int>());
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
 
-	o->connect(i);
+    o->connect(i);
 
-	// input's source address should equal output's address
-	EXPECT_EQ(o.get(), i->source.lock().get());
-	// output should have exactly one consumer
-	EXPECT_EQ(1, o->consumers.size());
-	// input's address should be referred to by output's first entry into consumers vector
-	EXPECT_EQ(i.get(), o->consumers[0].lock().get());
-	// both input and output should reference the same address with their data property
-	EXPECT_EQ(o->data.get(), i->data.lock().get());
+    // input's source address should equal output's address
+    EXPECT_EQ(o.get(), i->source.lock().get());
+    // output should have exactly one consumer
+    EXPECT_EQ(1, o->consumers.size());
+    // input's address should be referred to by output's first entry into consumers vector
+    EXPECT_EQ(i.get(), o->consumers[0].lock().get());
+    // both input and output should reference the same address with their data property
+    EXPECT_EQ(o->data.get(), i->data.lock().get());
+}
+
+TEST(Output, _Disconnect) {
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
+    o->connect(i);
+
+    o->_disconnect(i);
+
+    // input's source address should still equal output's address
+    EXPECT_EQ(o.get(), i->source.lock().get());
+    // output should now have zero consumers
+    EXPECT_EQ(0, o->consumers.size());
+    // both input and output should still reference the same address with their data property
+    EXPECT_EQ(o->data.get(), i->data.lock().get());
+}
+
+TEST(Output, Disconnect) {
+    auto i = std::make_shared<Input<int>>(Input<int>());
+    auto o = std::make_shared<Output<int>>(Output<int>());
+    o->connect(i);
+
+    o->disconnect(i);
+
+    // input's source address should be nullptr
+    EXPECT_EQ(nullptr, i->source.lock());
+    // output should now have zero consumers
+    EXPECT_EQ(0, o->consumers.size());
+    // both input and output should reference the same address with their data property
+    EXPECT_EQ(nullptr, i->data.lock());
 }
