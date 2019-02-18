a4cc805e58fe378ace383ad086a15bbf7d73e5c1
Jan <jan@hettenkofer.net>
2019-02-18 17:32:43 +0100

    [ broken ] further investigation into root cause of PPM malfunction

--- src/file_formats/pbm.h
+++ src/file_formats/pbm.h
@@ -12,35 +12,59 @@ struct ppm
     unsigned int maxval;
     uint8_t* data;
 
+    template<size_t channels>
+    ppm(matrix<pixel<channels>> m, unsigned int maxval = 256) {
+        width = m.width;
+        height = m.height;
+        maxval = maxval;
+
+        // calculate necessary space
+        size_t channel_size = maxval <= 256 ? 1 : 2;
+        size_t data_length = channel_size * sizeof(uint8_t) * 3 * m.width * m.height;
+        data = new uint8_t[data_length];
+        // write the image data
+        for (unsigned int x = 0; x < m.width; ++x) {
+            for (unsigned int y = 0; y < m.height; ++y) {
+                for (unsigned int channel = 0; channel < 3; ++channel) {
+                    data[y * m.width + x + channel] = channel <= channels ?
+                        // if there is data for this particular channel in the matrix, clamp it and multiply by maxval
+                        static_cast<uint8_t>(std::min(m.at(x, y)[channel], 1.f) * (maxval - 1)) :
+                        0; // otherwise just write 0
+                }
+            }
+        }
+    }
+
     ~ppm() {
         delete[] data;
     }
 
     template<size_t channels>
-    static ppm from_pixel_matrix(matrix<pixel<channels>> m, unsigned int maxval = 256) {
-        ppm img;
+    // static ppm from_pixel_matrix(matrix<pixel<channels>> m, unsigned int maxval = 256) {
+    //     ppm img;
 
-        img.width = m.width;
-        img.height = m.height;
-        img.maxval = maxval;
+    //     img.width = m.width;
+    //     img.height = m.height;
+    //     img.maxval = maxval;
 
-        // write the image data
-        size_t channel_size = img.maxval <= 256 ? 1 : 2;
-        size_t data_length = channel_size * sizeof(uint8_t) * 3 * m.width * m.height;
-        img.data = new uint8_t[data_length];
-        for (unsigned int x = 0; x < m.width; ++x) {
-            for (unsigned int y = 0; y < m.height; ++y) {
-                for (unsigned int channel = 0; channel < 3; ++channel) {
-                    img.data[y * m.width + x + channel] = channel <= channels ?
-                        // if there is data for this particular channel in the matrix, clamp it and multiply by maxval
-                        std::min(m.at(x, y)[channel], 1.f) * (img.maxval - 1) :
-                        0; // otherwise just write 0
-                }
-            }
-        }
+    //     // calculate necessary space
+    //     size_t channel_size = img.maxval <= 256 ? 1 : 2;
+    //     size_t data_length = channel_size * sizeof(uint8_t) * 3 * m.width * m.height;
+    //     img.data = new uint8_t[data_length];
+    //     // write the image data
+    //     for (unsigned int x = 0; x < m.width; ++x) {
+    //         for (unsigned int y = 0; y < m.height; ++y) {
+    //             for (unsigned int channel = 0; channel < 3; ++channel) {
+    //                 img.data[y * m.width + x + channel] = channel <= channels ?
+    //                     // if there is data for this particular channel in the matrix, clamp it and multiply by maxval
+    //                     static_cast<uint8_t>(std::min(m.at(x, y)[channel], 1.f) * (img.maxval - 1)) :
+    //                     0; // otherwise just write 0
+    //             }
+    //         }
+    //     }
 
-        return img;
-    }
+    //     return img;
+    // }
 
     matrix<pixel<3>> to_pixel_matrix() {
         matrix<pixel<3>> m(width, height);
--- tests/file_formats/pbm.test.cpp
+++ tests/file_formats/pbm.test.cpp
@@ -4,22 +4,30 @@
 
 #include "../../src/file_formats/pbm.h"
 
-TEST(PPM, from_pixel_matrix) {
+TEST(PPM, ConstructorFromPixelMatrix) {
     const matrix<pixel<4>> m(4, 2);
 
-    auto img = ppm::from_pixel_matrix<4>(m);
+    for (auto pxl: m.data) {
+        pxl[0] = 0.42;
+        pxl[1] = 0.42;
+        pxl[2] = 0.42;
+        pxl[3] = 0.42;
+    }
+
+    ppm img = m;
     EXPECT_EQ(4, img.width);
     EXPECT_EQ(2, img.height);
     EXPECT_EQ(256, img.maxval);
     for (size_t i = 0; i < img.width * img.height * 3; ++i) {
-        EXPECT_EQ(0, img.data[i]);
+        // 107 is 0.42 * 255 rounded to an int
+        EXPECT_EQ(107, img.data[i]);
     }
 }
 
 TEST(PPM, to_pixel_matrix) {
     const matrix<pixel<4>> m(4, 2);
 
-    auto img = ppm::from_pixel_matrix<4>(m);
+    auto img = ppm<4>(m);
 
     auto result = img.to_pixel_matrix();
 
@@ -37,24 +45,24 @@ TEST(PPM, StreamInsertion) {
 }
 
 TEST(PPM, StreamExtraction) {
-    std::ifstream file;
-    file.open("../_data/checker_3x3.pbm", std::ios::binary);
+    // std::ifstream file;
+    // file.open("../_data/checker_3x3.pbm", std::ios::binary);
 
-    ppm img;
+    // ppm img;
 
-    try {
-        file >> img;
+    // try {
+    //     file >> img;
 
-        EXPECT_EQ('P', img.magic_number[0]);
-        EXPECT_EQ('6', img.magic_number[1]);
-        EXPECT_EQ(3, img.width);
-        EXPECT_EQ(3, img.height);
-        EXPECT_EQ(256, img.maxval);
-        for (unsigned int i = 0; i < img.width * img.height * 3; ++i) {
-            EXPECT_EQ((i / 3) % 2 == 0 ? 0 : 255, img.data[i]);
-        }
-    } catch (char const * err) {
-        fprintf(stderr, "%s\n", err); 
-        file.close();
-    }
+    //     EXPECT_EQ('P', img.magic_number[0]);
+    //     EXPECT_EQ('6', img.magic_number[1]);
+    //     EXPECT_EQ(3, img.width);
+    //     EXPECT_EQ(3, img.height);
+    //     EXPECT_EQ(256, img.maxval);
+    //     for (unsigned int i = 0; i < img.width * img.height * 3; ++i) {
+    //         EXPECT_EQ((i / 3) % 2 == 0 ? 0 : 255, img.data[i]);
+    //     }
+    // } catch (char const * err) {
+    //     fprintf(stderr, "%s\n", err); 
+    //     file.close();
+    // }
 }
