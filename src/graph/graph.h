e3f4dcd074ae152f16396689ecb8decfa6931670
Jan Hettenkofer <jan@hettenkofer.net>
2019-03-22 09:42:36 +0100

    [ untested ] added register()/deregister() to graph

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -7,7 +7,31 @@
 
 class graph {
 private:
-    std::vector<primitive_node> p_nodes;
+    std::vector<std::shared_ptr<primitive_node>> p_nodes;
+
+public:
+    /**
+     * Will add the node to the internal list of managed nodes.
+     * Returns `false` if the node could not be added (e.g. because it
+     * was already present) and `true` otherwise.
+     */
+    bool register_node(std::shared_ptr<primitive_node> node) {
+        if (std::find(p_nodes.begin(), p_nodes.end(), node) != p_nodes.end()) return false;
+        p_nodes.push_back(node);
+        return true;
+    }
+
+    /**
+     * Removes the node from the list of managed nodes.
+     * Returns `false` if the node could not be removed (e.g. because it
+     * could not be found) and `true` otherwise.
+     */
+    bool deregister_node(std::shared_ptr<primitive_node> node) {
+        auto found = std::find(p_nodes.begin(), p_nodes.end(), node);
+        if (found == p_nodes.end()) return false;
+        p_nodes.erase(found);
+        return true;
+    }
 };
 
 #endif // SPICE_GRAPH
