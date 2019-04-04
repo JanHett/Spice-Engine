d185faffa8b72c147cd4728a0b21659edc72dff6
Jan Hettenkofer <jan@hettenkofer.net>
2019-04-04 21:59:07 +0200

    Refactored graph to fully own all the nodes it contains

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -7,8 +7,8 @@
 
 class graph {
 private:
-    std::vector<std::shared_ptr<basic_node>> p_nodes;
-    std::vector<std::shared_ptr<basic_node>> p_data_sinks;
+    std::vector<std::unique_ptr<basic_node>> p_nodes;
+    std::vector<basic_node*> p_data_sinks;
 
 public:
     /**
@@ -18,16 +18,17 @@ public:
         class NodeT,
         class... Args,
         std::enable_if_t<std::is_constructible<NodeT, Args&&...>::value, int> = 0>
-    std::shared_ptr<NodeT> create_node(Args&&... args)
+    NodeT& create_node(Args&&... args)
     {
         static_assert(std::is_base_of<basic_node, NodeT>::value,
             "create_node() can only create types derived from node");
 
-        auto n = std::make_shared<NodeT>(std::forward<Args>(args)...);
+        auto n = std::make_unique<NodeT>(std::forward<Args>(args)...);
+        auto n_ptr = n.get();
 
-        register_node(n);
+        register_node(std::move(n));
 
-        return n;
+        return *n;
     }
 
     /**
@@ -35,14 +36,16 @@ public:
      * Returns `false` if the node could not be added (e.g. because it
      * was already present) and `true` otherwise.
      */
-    bool register_node(std::shared_ptr<basic_node> node)
+    bool register_node(std::unique_ptr<basic_node> node)
     {
         if (std::find(p_nodes.begin(), p_nodes.end(), node) != p_nodes.end())
             return false;
-        p_nodes.push_back(node);
+        
         // TODO: make this constexpr
         if (node->is_data_sink)
-            p_data_sinks.push_back(node);
+            p_data_sinks.push_back(node.get());
+        p_nodes.push_back(std::move(node));
+        
         return true;
     }
 
@@ -51,21 +54,23 @@ public:
      * Returns `false` if the node could not be removed (e.g. because it
      * could not be found) and `true` otherwise.
      */
-    bool deregister_node(std::shared_ptr<basic_node> node)
+    bool deregister_node(basic_node& node)
     {
         // if this is a data sink, we don't return true unless it was also
         // removed from data_sinks.
-        if (node->is_data_sink) { // TODO: make this contexpr
+        if (node.is_data_sink) { // TODO: make this contexpr
             auto found_in_data_sinks = std::find(
                 p_data_sinks.begin(),
                 p_data_sinks.end(),
-                node);
+                &node);
             if (found_in_data_sinks == p_data_sinks.end())
                 return false;
             p_data_sinks.erase(found_in_data_sinks);
         }
 
-        auto found = std::find(p_nodes.begin(), p_nodes.end(), node);
+        auto found = std::find_if(p_nodes.begin(), p_nodes.end(),
+            [&node](std::unique_ptr<basic_node> const & arg){
+                return arg.get() == &node; });
         if (found == p_nodes.end())
             return false;
         p_nodes.erase(found);
@@ -78,7 +83,7 @@ public:
         return p_nodes.size();
     }
 
-    std::vector<std::shared_ptr<basic_node>> const& data_sinks()
+    std::vector<basic_node*> const& data_sinks()
     {
         return p_data_sinks;
     }
